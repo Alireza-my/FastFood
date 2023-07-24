05b4956f3fc76f554de3e0708753beb6265e49f8
Alireza-my <alireza.mahmoodi.amai@gmail.com>
2023-07-24 10:02:50 +0330

    Building AppLayout

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -1,5 +1,19 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
